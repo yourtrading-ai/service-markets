56915dda4e752d35a2bdff193d56d5395db10221
mhh <mike.hukiewitz@robotter.ai>
2023-07-07 18:41:07 +0200

    create new data model for service-markets

--- src/fishnet_cod/core/model.py
+++ src/fishnet_cod/core/model.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Any, Dict, List, Optional, Tuple
+from typing import List, Optional
 
 from aars import Record
 
@@ -12,102 +12,42 @@ class UserInfo(Record):
     link: Optional[str]
 
 
-class Timeseries(Record):
+class Votable(Record):
+    upvotes: int
+    downvotes: int
+
+
+class Service(Votable):
     name: str
-    owner: str
-    desc: Optional[str]
-    available: bool = True
-    data: List[Tuple[int, float]]
-    min: Optional[float]
-    max: Optional[float]
-    avg: Optional[float]
-    std: Optional[float]
-    median: Optional[float]
+    description: str
+    url: str
+    tags: List[str]
+    owner_address: str
 
 
-# Check coinmarketcap.com for the exact granularity/aggregation timeframes
-class Granularity(str, Enum):
-    DAY = "DAY"  # 1 value every five minutes
-    WEEK = "WEEK"  # 1 value every 15 minutes
-    MONTH = "MONTH"  # 1 value every hour
-    THREE_MONTHS = "THREE_MONTHS"  # 1 value every 3 hours
-    YEAR = "YEAR"  # 1 value every day
+class VoteType(Enum):
+    UPVOTE = 1
+    DOWNVOTE = -1
 
 
-class View(Record):
-    startTime: int
-    endTime: int
-    granularity: Granularity
-    columns: List[str]
-    values: Dict[str, List[Tuple[int, float]]]  # timeseriesID -> data
+class VotableType(Enum):
+    SERVICE = "service"
+    COMMENT = "comment"
 
 
-class Dataset(Record):
-    name: str
-    owner: str
-    ownsAllTimeseries: bool
-    available: bool = True
-    timeseriesIDs: List[str]
-    desc: Optional[str]
-    viewIDs: Optional[List[str]]
+class Vote(Record):
+    item_id: str
+    item_type: VotableType
+    user_address: str
+    vote: VoteType
 
 
-class Algorithm(Record):
-    name: str
-    desc: str
-    owner: str
-    code: str
-
-
-class ExecutionStatus(str, Enum):
-    REQUESTED = "REQUESTED"
-    PENDING = "PENDING"
-    DENIED = "DENIED"
-    RUNNING = "RUNNING"
-    SUCCESS = "SUCCESS"
-    FAILED = "FAILED"
-
-
-class Execution(Record):
-    algorithmID: str
-    datasetID: str
-    owner: str
-    status: ExecutionStatus = ExecutionStatus.REQUESTED
-    params: Optional[dict]
-
-
-class PermissionStatus(str, Enum):
-    REQUESTED = "REQUESTED"
-    GRANTED = "GRANTED"
-    DENIED = "DENIED"
+class Comment(Votable):
+    service_id: str
+    user_address: str
+    comment: str
 
 
 class Permission(Record):
-    """
-    A permission request for a dataset.
-    """
-
-    authorizer: str
-    requestor: str
-    datasetID: str
-    """
-    The datasetID that the permission was requested for.
-    Can be a different dataset than the one the timeseries belongs to.
-    If the dataset is a composite dataset, this is the composite dataset and timeseriesID CANNOT be None.
-    This is because multiple permissions of multiple users are potentially required for a composite dataset.
-    """
-    timeseriesID: Optional[str]
-    """
-    The timeseriesID that the permission was requested for.
-    """
-    algorithmID: Optional[str]
-    status: PermissionStatus
-    executionCount: int
-    maxExecutionCount: Optional[int]
-
-
-class Result(Record):
-    executionID: str
-    owner: str
-    executor_vm: str
-    data: Any
+    user_address: str
+    service_id: str
